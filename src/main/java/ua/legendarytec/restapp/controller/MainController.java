69569f7ce1bf321d7f3339b18a0e13c6097655db
glebshvydkov <glebkogo@gmail.com>
2019-01-10 09:45:22 +0200

    Implemented Node -> Webpack. Refactored view templated.

--- src/main/java/ua/legendarytec/restapp/controller/MainController.java
+++ src/main/java/ua/legendarytec/restapp/controller/MainController.java
@@ -1,6 +1,7 @@
 package ua.legendarytec.restapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,9 @@ public class MainController {
 
     private final MessageRepo messageRepo;
 
+    @Value("${spring.profile.active}")
+    private String profile;
+
     @Autowired
     public MainController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
@@ -30,6 +34,8 @@ public class MainController {
         data.put("messages", messageRepo.findAll());
 
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode",true);
+
         return "index";
     }
 }
